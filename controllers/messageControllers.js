2955ba7af248300dc04838fb1ee5a69ddd3e39c1
EljerPenny <elijahjpenny@gmail.com>
2023-02-01 00:32:36 -0500

    Added message delete route

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -15,6 +15,7 @@ const router = express.Router()
 ////      Routes         ////
 /////////////////////////////
 
+//Create Route
 router.post('/:receiverId', (req,res)=> {
     const receiverId = req.params.receiverId
     req.body.recipient = receiverId
@@ -49,8 +50,30 @@ router.get('/:receiverId', (req,res) => {
 		})
 })
 
-//Create route
-router.get('/')
+//Delete Route
+router.delete('/:id', (req, res)=>{
+    const id = req.params.id
+    Message.findById(id)
+        .then(message=> {
+            const recipient = message.recipient
+            console.log('this is the message', message)
+            // if the author of the message is the person who is logged in
+            if (message.author == req.session.userId) {
+                // send success message
+                //res.sendStatus(204)
+                // delete the fruit
+                res.redirect(`/message/${recipient}`)
+                return message.deleteOne()
+            } else {
+                // res.sendStatus(401)
+                res.redirect(`/error?error=You%20Are%20not%20allowed%20to%20delete%20this%20fruit`)
+            }
+        })
+        .catch(err => {
+            // res.status(400).json(err)
+            res.redirect(`/error?error=${err}`)
+        })
+})
 
 //////////////////////////////
 //// Export Router        ////
